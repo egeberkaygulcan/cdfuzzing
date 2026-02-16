148490fbb7fc2ba9bada59d651bddd95481344f1
Ege Berkay Gulcan <egeberkaygulcan@gmail.com>
2026-02-16 22:24:50 +0100

    update

---
+++ newsrc/fetch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env FUZZER: path to fuzzer work dir
+##
+
+git clone --no-checkout https://github.com/google/AFL.git "$FUZZER/repo"
+git -C "$FUZZER/repo" checkout 61037103ae3722c8060ff7082994836a794f978e
+
+#wget -O "$FUZZER/repo/afl_driver.cpp" \
+#    "https://cs.chromium.org/codesearch/f/chromium/src/third_party/libFuzzer/src/afl/afl_driver.cpp"
+
+cp "$FUZZER/src/afl_driver.cpp" "$FUZZER/repo/afl_driver.cpp"
+
+# Copy drift detection modifications
+cp "$FUZZER/newsrc/afl-drift-detect.c" "$FUZZER/repo/afl-drift-detect.c"
+cp "$FUZZER/newsrc/afl-drift-detect.h" "$FUZZER/repo/afl-drift-detect.h"
+cp "$FUZZER/newsrc/afl-fuzz.c" "$FUZZER/repo/afl-fuzz.c"
+cp "$FUZZER/newsrc/Makefile" "$FUZZER/repo/Makefile"
